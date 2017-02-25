cd9621d59bc78f38c93c0d0b90faa641fcfaca60
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-24 23:41:37 -0300

    Exercícios sobre métodos através da apostila da Caelum.

---
+++ App - Restaurante/restaurante_avancado.rb
@@ -0,0 +1,15 @@
+#
+# Arquivo: restaurante_basico.rb
+# Author: Glaucia Lemos
+# Data: 24/02/2017
+# Observação: exercícios baseado na apostila da Caelum
+#
+
+# Exercício: 4.4 - Métodos:
+
+# declaração do método
+def qualifica(nota)
+	puts "A nota do restaurante foi #{nota}"
+end
+
+qualifica(10)
