33904af07e1206d5f3f761929afe10b8c54e587f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-24 23:10:14 -0300

    Exercícios da apostila da Caelum.

--- App - Restaurante/restaurante_basico.rb
+++ App - Restaurante/restaurante_basico.rb
@@ -8,8 +8,16 @@
 # Exercício: 3.6 - Variáveis e Atribuições
 # Link: https://www.caelum.com.br/apostila-ruby-on-rails/ruby-basico/#3-6-exercicios-variaveis-e-atribuicoes
 
+=begin
 puts 'Digite o nome do restaurante: '
 nome = gets
 
 printf 'Nome do restaurante: '
 puts nome
+=end
+
+# Exercício: 3.8 - Strings
+puts 'Digite o nome do restaurante: '
+nome = gets
+
+print 'Nome do restaurante: ' + nome
