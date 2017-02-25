c04006f8cdbce395f2d98d49df78e46f25379450
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-24 21:40:34 -0300

    Exercícios sobre operadores.

---
+++ Aula - 05/Operadores.rb
@@ -0,0 +1,43 @@
+#
+# Aula: 5 - Operadores
+# Author: Glaucia Lemos
+# Data: 24/02/2017
+# Observação: (exemplos realizados via terminal (cmd))
+#
+
+# Exemplo de Calculadora:
+
+num1 = 1
+num2 = 0
+#total = 0
+
+
+# Primeira entrada:
+#puts 'Digite o Primeiro Número :'
+#num1 = STDIN.gets
+#num1 = num1.to_i
+
+# Segunda entrada:
+#puts 'Digite o Segundo Número :'
+#num2 = STDIN.gets
+#num2 = num2.to_i
+
+#total = num1 + num2
+#total = num1 - num2
+#total = num1 * num2
+#total = num1 / num2
+#total = num1 ** num2 #exponenciação
+
+# Saída
+#puts 'Resultado...: ' + total.to_s
+
+# Exemplo de Operadores de Comparação:
+
+#puts num1 == num2
+#puts num1 != num2
+#puts num1.equal? num2
+#puts num1.eql? num2
+
+#puts num1 > num2
+puts num1 <=> num2
+
