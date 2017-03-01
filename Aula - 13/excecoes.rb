bb64c158d1e9934e894a1ea3aa9f72fa3d45080f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-03-01 00:23:17 -0300

    Exercícios sobre Exceçoes em Ruby.

---
+++ Aula - 13/excecoes.rb
@@ -0,0 +1,25 @@
+#
+# Aula: 13 - Exceções
+# Author: Glaucia Lemos
+# Data: 28/02/2017
+# Observação: (exemplos realizados via terminal (cmd))
+#
+
+# def excep
+# 	raise TypeError, 'Você deve ter cuidado...' # raise = thrown exception
+# end
+
+# excep()
+
+# Exemplo de try-catch em Ruby:
+
+# begin
+# 	puts 'Este é um try'
+# 	raise TypeError, 'Este é um exception'
+# rescue Exception => e
+# 	puts e.message
+# 	puts e.backtrace
+# end
+
+
+
