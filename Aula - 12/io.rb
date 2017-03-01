7068217d6e2eda75326d11fb8bc3476649f99136
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-03-01 00:11:16 -0300

    Exercícios sobre IO/Input em Ruby.

---
+++ Aula - 12/io.rb
@@ -0,0 +1,25 @@
+#
+# Aula: 12 - IO
+# Author: Glaucia Lemos
+# Data: 28/02/2017
+# Observação: (exemplos realizados via terminal (cmd))
+#
+
+# fname = 'teste.txt'
+
+# file = File.open(fname, 'w') # w = write
+
+# file.puts 'Testando o IO via Ruby... será que vai funfar!!! Tomara!!!'
+
+# file.close
+
+# file = File.open('teste.txt', 'r') # r = read
+# contents = file.read
+# puts contents
+
+# File.open('teste.txt').readlines.each do |linhas|
+# 	puts linhas
+# end
+
+# puts Dir.pwd
+# Dir.mkdir('files') unless File.exists? ('files')
